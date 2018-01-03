80267d34d144d972bef5311f99a21c0ce1160633
Christopher Diep <diep.christopher@gmail.com>
2018-01-03 07:43:01 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,25 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |hash_array|
+    hash_array.each do |k,v|
+      if v == "Winner"
+        return hash_array["name"].split(" ")[0]
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season,data_array|
+    data_array.each do |person_info|
+      person_info.each do |k,v|
+        if v == occupation
+          return person_info["name"]
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
